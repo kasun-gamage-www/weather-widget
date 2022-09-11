527c18e6395384d11d58ee888c90f97b3525d138
Anupama Gamage <kasun.anupama92@gmail.com>
2022-09-11 18:26:27 +0530

    icon mapping. main section styling

--- src/app/@core/util/icon-mapping.ts
+++ src/app/@core/util/icon-mapping.ts
@@ -1,9 +1,9 @@
-export default class IconMapping{
-    static getIconURIByCode(){
-        return ``
+export default class IconMapping {
+    static getIconURIByCode(code: any) {
+        return `https://developer.accuweather.com/sites/default/files/0${code}-s.png`
     }
-    
-    static getIconURIByText(){
-        
+
+    static getIconURIByText(code : any) {
+        return `../assets/img/${ code || 'Clouds' }.png`
     }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 import { MockWeatherService } from './@core/services/mock/mock-weather.service'
 import WeatherService from './@core/services/weather.service'
 import Geo from './@core/util/geo'
+import IconMapping from './@core/util/icon-mapping'
 
 @Component({
   selector: 'app-root',
@@ -20,11 +21,13 @@ export class AppComponent {
   /*Data - static*/
   weatherData: any | null = null
   forecastDays = [...Array(5).keys()]
+  conditionIcon : string | undefined
 
   constructor(private weather: MockWeatherService) {
     weather.getCurrentCondition(null).subscribe(weatherData => {
       console.log(weatherData)
       this.weatherData = weatherData
+      this.conditionIcon = IconMapping.getIconURIByCode(weatherData[0]['WeatherIcon'])
     })
 
     Geo.fetchUserLocation(position => {
